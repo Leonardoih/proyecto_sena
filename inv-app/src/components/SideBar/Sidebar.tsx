86c2c3f01f1b322622f399345da3d25b2ea270e7
leonardoih <leonardoih090217@gamil.com>
2022-01-05 23:17:15 -0500

    agegando el navbar y el side bar

--- inv-app/src/components/SideBar/Sidebar.tsx
+++ inv-app/src/components/SideBar/Sidebar.tsx
@@ -1,11 +1,111 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-    return (
-        <div>
-            <h3>Este es el Side Bar</h3>
-        </div>
-    )
-}
+  return (
+    <div
+      className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark"
+      style={{ width: "280px", height: "700px" }}
+    >
+      <Link
+        to="/"
+        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+      >
+          
+        <span className="fs-4">Sidebar</span>
+      </Link>
+      <hr />
+      <ul className="nav nav-pills flex-column mb-auto">
+        <li className="nav-item">
+          <Link to="#" className="nav-link text-white" aria-current="page">
+            <svg className="bi me-2" width="16" height="16">
+              <use to="#home" />
+            </svg>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="#" className="nav-link text-white">
+            <svg className="bi me-2" width="16" height="16">
+              <use to="#speedometer2" />
+            </svg>
+            Dashboard55
+          </Link>
+        </li>
+        <li>
+          <Link to="#" className="nav-link text-white">
+            <svg className="bi me-2" width="16" height="16">
+              <use to="#table" />
+            </svg>
+            Orders
+          </Link>
+        </li>
+        <li>
+          <Link to="#" className="nav-link text-white">
+            <svg className="bi me-2" width="16" height="16">
+              <use to="#grid" />
+            </svg>
+            Products
+          </Link>
+        </li>
+        <li>
+          <Link to="#" className="nav-link text-white">
+            <svg className="bi me-2" width="16" height="16">
+              <use to="#people-circle" />
+            </svg>
+            Customers
+          </Link>
+        </li>
+      </ul>
+      <hr />
+      <div className="dropdown">
+        <Link
+          to="#"
+          className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+          id="dropdownUser1"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <img
+            src="https://github.com/mdo.png"
+            alt=""
+            width="32"
+            height="32"
+            className="rounded-circle me-2"
+          />
+          <strong>mdo</strong>
+        </Link>
+        <ul
+          className="dropdown-menu dropdown-menu-dark text-small shadow"
+          aria-labelledby="dropdownUser1"
+        >
+          <li>
+            <Link className="dropdown-item" to="#">
+              New project...
+            </Link>
+          </li>
+          <li>
+            <Link className="dropdown-item" to="#">
+              Settings
+            </Link>
+          </li>
+          <li>
+            <Link className="dropdown-item" to="#">
+              Profile
+            </Link>
+          </li>
+          <li>
+            <hr className="dropdown-divider" />
+          </li>
+          <li>
+            <Link className="dropdown-item" to="#">
+              Sign out
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- inv-app/src/pages/Dashboar.tsx
+++ inv-app/src/pages/Dashboar.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Sidebar from "../components/SideBar/Sidebar";
 
+
 const Dashboar = () => {
   return (
     <div className="container">
         <div>
             <Navbar></Navbar>
-          <h1>Dashboard11 !!!</h1>
+          <h1>Inv-App</h1>
           <Sidebar></Sidebar>
         </div>
     </div>
